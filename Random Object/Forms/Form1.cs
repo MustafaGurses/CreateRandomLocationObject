d7dd387210d9163556f217c0b0e25906b07bdd64
Mustafa Gürses <36699074+MustafaGurses@users.noreply.github.com>
2020-11-09 16:05:47 +0300

    Add files via upload

---
+++ Random Object/Forms/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Random_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button_ = new Object.Button_();
+        }
+        Object.Button_ button_;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button_.Windows_Height = this.Height;
+            button_.Windows_Width = this.Width;
+            this.Controls.Add(button_.create_button(new Point(0,0)));
+        }
+    }
+}
---
+++ Random Object/Object/Button_.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace Random_Game.Object
+{
+    public class Button_
+    {
+        public int Windows_Height { get; set; }
+        public int Windows_Width { get; set; }
+        private static string object_name { get; set; }
+        public Button create_button(System.Drawing.Point location)
+        {
+            Button create_ = new Button();
+            create_.Name = "test_" + DateTime.Now.Second;
+            create_.Location = location;
+            create_.Click += Create__Click;
+            create_.Size = new System.Drawing.Size(100, 100);
+            create_.Text = "Click me";
+            object_name = create_.Name;
+            return create_;
+        }
+
+        private void Create__Click(object sender, EventArgs e)
+        {
+            Application.OpenForms["Form1"].Controls.Find(object_name, true)[0].Dispose();
+            Windows_Height = Application.OpenForms["Form1"].Size.Height;
+            Windows_Width = Application.OpenForms["Form1"].Size.Width;
+            Application.OpenForms["Form1"].Controls.Add(create_button(create_object_location(Windows_Height,Windows_Width)));
+        }
+        private System.Drawing.Point create_object_location(int windows_height,int windows_width)
+        {
+            System.Drawing.Point point = new System.Drawing.Point();
+            Random random = new Random();
+            point.Y = random.Next(0, windows_height-150);
+            point.X = random.Next(0, windows_width-150);
+            return point;
+        }
+    }
+}
